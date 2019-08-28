e6dcee0725af768118d6be398c04f7d9b95a0d71
海比丶天蓝 <149352394@qq.com>
2019-08-29 00:54:32 +0800

    TW 85.06

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ module.exports = function BossHelper(mod) {
 		bossHunting = 0,
 		bossTemplate = 0
 	
-	mod.command.add("怪物", (arg) => {
+	mod.command.add(["boss", "怪物"], (arg) => {
 		if (!arg) {
 			mod.settings.enabled = !mod.settings.enabled
 			MSG.chat("Boss-Helper " + (mod.settings.enabled ? MSG.BLU("开启") : MSG.YEL("关闭")))
@@ -92,6 +92,29 @@ module.exports = function BossHelper(mod) {
 			if (mod.settings.notice) {
 				MSG.raids("发现 " + boss.name)
 			}
+		}
+		
+		if (event.walkSpeed != 240) return;
+		
+		switch (event.templateId) {
+			case 5001: // Ortan
+				event.shapeId = 303730;
+				event.templateId = 7000;
+				event.huntingZoneId = 434;
+				load(event);
+				return true;
+			case 501:  // Hazard
+				event.shapeId = 303740;
+				event.templateId = 77730;
+				event.huntingZoneId = 777;
+				load(event);
+				return true;
+			case 4001: // Cerrus
+				event.shapeId = 303750;
+				event.templateId = 1000;
+				event.huntingZoneId = 994;
+				load(event);
+				return true;
 		}
 	})
 	
@@ -191,33 +214,15 @@ module.exports = function BossHelper(mod) {
 			boss = null
 		}
 	}
-	/* 
-	mod.hook('S_TARGET_INFO', 1, (event) => {
-		if (mobid.includes(event.target)) {
-			console.log("S_TARGET_INFO - HP-Level")
-			console.log(event.hp)
-			console.log("------")
-		}
-	})
 	
-	mod.hook('S_BOSS_GAGE_INFO', 3, (event) => {
-		if (mobid.includes(event.id)) {
-			console.log("S_BOSS_GAGE_INFO - HP-Level")
-			console.log(event.curHp/event.maxHp)
-		}
-	})
-	 */
 	function spawnItem(gameId, loc) {
 		loc.z = loc.z - 100
-		mod.send('S_SPAWN_DROPITEM', 7, {
+		mod.send('S_SPAWN_DROPITEM', 8, {
 			gameId: gameId*100n,
 			loc: loc,
 			item: mod.settings.itemId,
 			amount: 1,
-			expiry: 999999,
-			owners: [
-				{playerId: mod.game.me.playerId}
-			]
+			expiry: 999999
 		})
 	}
 	
@@ -225,6 +230,85 @@ module.exports = function BossHelper(mod) {
 		mod.send('S_DESPAWN_DROPITEM', 4, {
 			gameId: gameId*100n
 		})
+	}
+	
+	// BAM-HP-Bar
+	let gage_info = {
+			id: 0n,
+			huntingZoneId: 0,
+			templateId: 0,
+			target: 0n,
+			unk1: 0,
+			unk2: 0,
+			curHp: 16000000000n,
+			maxHp: 16000000000n,
+			unk3: 1
+		},
+		hooks = []
+	
+	
+	function update_hp() {
+		mod.toClient('S_BOSS_GAGE_INFO', 3, gage_info);
+	}
+	// 0: 0% <= hp < 20%, 1: 20% <= hp < 40%, 2: 40% <= hp < 60%, 3: 60% <= hp < 80%, 4: 80% <= hp < 100%, 5: 100% hp
+	function correct_hp(stage) {
+		let boss_hp_stage = BigInt(20*(1+stage));
+		// we missed some part of the fight?
+		if (gage_info.curHp * 100n / gage_info.maxHp > boss_hp_stage) {
+			gage_info.curHp = gage_info.maxHp * boss_hp_stage / 100n;
+			update_hp();
+			mod.command.message('Correcting boss hp down to <font color="#E69F00">' + String(boss_hp_stage) + '</font>%');
+		}
+	}
+	
+	function load(e) {
+		gage_info.id = e.gameId;
+		gage_info.curHp = gage_info.maxHp;
+		correct_hp(e.hpLevel);
+		if (e.mode) {
+			mod.command.message('You missed ~ <font color="#E69F00">' + Math.round((99999999 - e.remainingEnrageTime)/1000) + '</font> sec. of the fight');
+		}
+		
+		if (e.hpLevel == 5) {
+			mod.command.message("BAM is at full 100% hp, nobody touched it");
+		} else if (e.hpLevel == 0) {
+			mod.command.message("BAM is likely far below 20% hp, it may die any moment now");
+		}
+		
+		if (!hooks.length) {
+			setTimeout(update_hp, 1000);
+			hook('S_NPC_STATUS', 2, (event) => {
+				if (event.gameId === gage_info.id) {
+					correct_hp(event.hpLevel);
+				}
+			});
+			
+			hook('S_EACH_SKILL_RESULT', 13, (event) => {
+				if (event.target === gage_info.id && event.type === 1) {
+					gage_info.curHp -= event.value;
+					update_hp();
+				}
+			});
+			
+			hook('S_DESPAWN_NPC', 3, (event) => {
+				if (event.gameId === gage_info.id) {
+					unload();
+				}
+			});
+		}
+	}
+	
+	function unload() {
+		if (hooks.length) {
+			for (let h of hooks) {
+				mod.unhook(h);
+			}
+			hooks = []
+		}
+	}
+	
+	function hook() {
+		hooks.push(mod.hook(...arguments));
 	}
 	
 }
