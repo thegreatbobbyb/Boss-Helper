5709f0041b5213cb1562849cbb5d14ec184f839d
海比丶天蓝 <149352394@qq.com>
2019-09-23 23:23:56 +0800

    TW 85.07

--- settings_migrator.js
+++ settings_migrator.js
@@ -33,12 +33,12 @@ const DefaultSettings = {
         {huntingZoneId: 39,   templateId: 501,  name: "哈迦叻(拓荒村莊)", killedTime: null, nextTime: null},
         {huntingZoneId: 51,   templateId: 4001, name: "蓋洛司(希望村莊)", killedTime: null, nextTime: null},
 
-        // 贝里卡
-{huntingZoneId:   2,   templateId: 1271, name: "[城镇 2-1271] 阿勒坤(西部砍伐區)"},
-{huntingZoneId:   3,   templateId: 1271, name: "[城镇 3-1271] 阿勒坤(遺忘森林)"},
-{huntingZoneId:   5,   templateId: 1271, name: "[城镇 5-1271] 阿勒坤(候季安保護區)"}, // 候季安濕地
-{huntingZoneId:   6,   templateId: 1271, name: "[城镇 6-1271] 阿勒坤(咆哮侵略地)"},
-{huntingZoneId:   7,   templateId: 1271, name: "[城镇 7-1271] 阿勒坤(永恆盆地)"}, // 學識丘陵
+        // 亚伦南部
+{huntingZoneId:   2,   templateId: 1271, name: "[2-1271] 阿勒坤(西部砍伐區)"},
+{huntingZoneId:   3,   templateId: 1271, name: "[3-1271] 阿勒坤(遺忘森林)"},
+{huntingZoneId:   5,   templateId: 1271, name: "[5-1271] 阿勒坤(候季安保護區)"}, // 候季安濕地
+{huntingZoneId:   6,   templateId: 1271, name: "[6-1271] 阿勒坤(咆哮侵略地)"},
+{huntingZoneId:   7,   templateId: 1271, name: "[7-1271] 阿勒坤(永恆盆地)"}, // 學識丘陵
 
         {huntingZoneId:   4,   templateId: 1271, name: "[東部領地]依托羅(爆炎山脈)"},
         {huntingZoneId:   9,   templateId: 1271, name: "[東部領地]依托羅(貪婪溪谷)"}, // 貪欲的黃金神殿
@@ -56,71 +56,71 @@ const DefaultSettings = {
         {huntingZoneId:  21,   templateId: 1271, name: "[比亞阿鳥雷溫領地]菲亞圖(蜥蜴王巢穴)"},
         {huntingZoneId:  24,   templateId: 1271, name: "[比亞阿鳥雷溫領地]菲亞圖(黃金之路)"},
 
-        // 尔雷曼西亚
+        // 夏拉南部
         {huntingZoneId:  26,   templateId: 1271, name: "[艾赛尼亚领地]艾沙特(生命溪谷)"},
         {huntingZoneId:  27,   templateId: 1271, name: "[艾赛尼亚领地]艾沙特(艾賽尼亞丘陵地)"}, // 艾賽尼亞丘陵地帶
         {huntingZoneId:  28,   templateId: 1271, name: "[艾赛尼亚领地]艾沙特(汙染森林)"},
         {huntingZoneId:  29,   templateId: 1271, name: "[艾赛尼亚领地]艾沙特(古代森林)"},
-        {huntingZoneId:  29,   templateId: 1272, name: "[城镇 29-1272] 艾沙特(古代森林)"},
-        // {huntingZoneId:  29,   templateId: 6100, name: "[城镇 29-6100] 艾沙特(古代森林)"},
-        // {huntingZoneId:  29,   templateId: 6101, name: "[城镇 29-6101] 艾沙特(古代森林)"},
+        {huntingZoneId:  29,   templateId: 1272, name: "[29-1272] 艾沙特(古代森林)"},
+        // {huntingZoneId:  29,   templateId: 6100, name: "[29-6100] 艾沙特(古代森林)"},
+        // {huntingZoneId:  29,   templateId: 6101, name: "[29-6101] 艾沙特(古代森林)"},
 
-{huntingZoneId:  30,   templateId: 1271, name: "[城镇 30-1271] 貝勒沙(太陽庭院)"}, // 眾神戰場
+{huntingZoneId:  30,   templateId: 1271, name: "[30-1271] 貝勒沙(太陽庭院)"}, // 眾神戰場
 
         {huntingZoneId:  31,   templateId: 1271, name: "[威斯多尼亞領地]斯托蘭(冰凍海岸)"}, // 暴風海岸
         {huntingZoneId:  32,   templateId: 1271, name: "[威斯多尼亞領地]斯托蘭(暴君高原)"},
         {huntingZoneId:  34,   templateId: 1271, name: "[威斯多尼亞領地]斯托蘭(寒冰凍原)"},
 
-{huntingZoneId:  35,   templateId: 1271, name: "[城镇 35-1271] 巴亞斯(古龍峽谷)"}, // 掠奪荒野
-{huntingZoneId:  36,   templateId: 1271, name: "[城镇 36-1271] 巴亞斯(驅逐者溪谷)"}, // 驅逐者之塔
-{huntingZoneId:  38,   templateId: 1271, name: "[城镇 37-1271] 巴亞斯(古代戰爭廢墟)"},
+{huntingZoneId:  35,   templateId: 1271, name: "[35-1271] 巴亞斯(古龍峽谷)"}, // 掠奪荒野
+{huntingZoneId:  36,   templateId: 1271, name: "[36-1271] 巴亞斯(驅逐者溪谷)"}, // 驅逐者之塔
+{huntingZoneId:  38,   templateId: 1271, name: "[37-1271] 巴亞斯(古代戰爭廢墟)"},
 
-{huntingZoneId:  40,   templateId: 1271, name: "[城镇 40-1271] 潘奈巴(隔離區)"},
-{huntingZoneId:  41,   templateId: 1271, name: "[城镇 41-1271] 潘奈巴(野蠻溪谷)"},
+{huntingZoneId:  40,   templateId: 1271, name: "[40-1271] 潘奈巴(隔離區)"},
+{huntingZoneId:  41,   templateId: 1271, name: "[41-1271] 潘奈巴(野蠻溪谷)"},
 
-        // 凯亚多勒
-{huntingZoneId:  45,   templateId: 1271, name: "[城镇 45-1271] 洛堤卡(青空溪谷)"}, // 傀儡要塞
-{huntingZoneId:  47,   templateId: 1271, name: "[城镇 47-1271] 洛堤卡(堤勒凱伊森林)"},
+        // 夏拉北部
+{huntingZoneId:  45,   templateId: 1271, name: "[45-1271] 洛堤卡(青空溪谷)"}, // 傀儡要塞
+{huntingZoneId:  47,   templateId: 1271, name: "[47-1271] 洛堤卡(堤勒凱伊森林)"},
 
-{huntingZoneId:  48,   templateId: 1271, name: "[城镇 48-1271] 海勒昆(卡諾比亞前線)"}, // 馬納安神殿
+{huntingZoneId:  48,   templateId: 1271, name: "[48-1271] 海勒昆(卡諾比亞前線)"}, // 馬納安神殿
 
-{huntingZoneId:  49,   templateId: 1271, name: "[城镇 49-1271] 洛卡農(卡勒古內雅)"},
-{huntingZoneId:  50,   templateId: 1271, name: "[城镇 50-1271] 洛卡農(阿勒古內雅)"},
+{huntingZoneId:  49,   templateId: 1271, name: "[49-1271] 洛卡農(卡勒古內雅)"},
+{huntingZoneId:  50,   templateId: 1271, name: "[50-1271] 洛卡農(阿勒古內雅)"},
 
-{huntingZoneId:  51,   templateId: 1271, name: "[城镇 51-1271] 洛亞昆(阿爾卡笛亞峽谷)"}, // 野獸高原
-{huntingZoneId:  52,   templateId: 1271, name: "[城镇 52-1271] 洛亞昆(遺忘者雪原)"}, // 使者戰場
+{huntingZoneId:  51,   templateId: 1271, name: "[51-1271] 洛亞昆(阿爾卡笛亞峽谷)"}, // 野獸高原
+{huntingZoneId:  52,   templateId: 1271, name: "[52-1271] 洛亞昆(遺忘者雪原)"}, // 使者戰場
 
         {huntingZoneId:  54,   templateId: 1271, name: "[希巴尼亞領地]希爾貝塔(妖精森林)"},
         {huntingZoneId:  55,   templateId: 1271, name: "[希巴尼亞領地]希爾貝塔(暗影森林)"},
         {huntingZoneId:  56,   templateId: 1271, name: "[希巴尼亞領地]希爾貝塔(黑暗森林)"},
         {huntingZoneId:  57,   templateId: 1271, name: "[希巴尼亞領地]希爾貝塔(阿梅納夸特拉)"},
 
-        // 伊露卡
-{huntingZoneId: 172,   templateId: 1271, name: "[城镇 172-1271] 巴雷谷(原始之地)"},
-{huntingZoneId: 181,   templateId: 1271, name: "[城镇 181-1271] 巴雷谷(古代巴拉卡遺跡)"},
-{huntingZoneId: 182,   templateId: 1271, name: "[城镇 182-1271] 巴雷谷(巴拉卡尼亞丘陵地)"},
-{huntingZoneId: 191,   templateId: 1271, name: "[城镇 191-1271] 巴雷谷(帝國之地)"},
+        // 亚伦北部
+{huntingZoneId: 172,   templateId: 1271, name: "[巴拉卡尼亞領地]巴雷谷(原始之地)"},
+{huntingZoneId: 181,   templateId: 1271, name: "[巴拉卡尼亞領地]巴雷谷(古代巴拉卡遺跡)"},
+{huntingZoneId: 182,   templateId: 1271, name: "[巴拉卡尼亞領地]巴雷谷(巴拉卡尼亞丘陵地)"},
+{huntingZoneId: 191,   templateId: 1271, name: "[巴拉卡尼亞領地]巴雷谷(帝國之地)"},
 
-{huntingZoneId: 183,   templateId: 1271, name: "[都市 183-1271] 約翰度(伊露卡)"},
-{huntingZoneId: 183,   templateId: 1278, name: "[都市 183-1278] 巴雷谷(伊露卡)"},
+{huntingZoneId: 183,   templateId: 1271, name: "[183-1271] 約翰度(伊露卡)"},
+{huntingZoneId: 183,   templateId: 1278, name: "[183-1278] 巴雷谷(伊露卡)"},
 
         // 直辖
         {huntingZoneId:  63,   templateId: 1271, name: "[貝利卡神秘商店]彼得佩依(貝利卡)"},
-// {huntingZoneId:  63,   templateId: 1272, name: "[都市 63-1272] 彼得佩依(貝利卡)"},
-// {huntingZoneId:  63,   templateId: 1273, name: "[都市 63-1273] 彼得佩依(貝利卡)"},
+// {huntingZoneId:  63,   templateId: 1272, name: "[63-1272] 彼得佩依(貝利卡)"},
+// {huntingZoneId:  63,   templateId: 1273, name: "[63-1273] 彼得佩依(貝利卡)"},
         {huntingZoneId:  63,   templateId: 1278, name: "[贝里克直辖领地]貝拉昆(貝利卡)"},
         {huntingZoneId:  63,   templateId: 1279, name: "[貝利卡神秘商店]彼得佩依(貝利卡)"},
-// {huntingZoneId:  63,   templateId: 1280, name: "[都市 63-1280] 彼得佩依(貝利卡)"},
-        {huntingZoneId:  63,   templateId: 1281, name: "[都市 63-1281] 彼得佩依(貝利卡)"},
-// {huntingZoneId:  63,   templateId: 2950, name: "[都市 63-2950] 彼得佩依(貝利卡)"},
-// {huntingZoneId:  63,   templateId: 2951, name: "[都市 63-2951] 彼得佩依(貝利卡)"},
-// {huntingZoneId:  63,   templateId: 2952, name: "[都市 63-2952] 彼得佩依(貝利卡)"},
+// {huntingZoneId:  63,   templateId: 1280, name: "[63-1280] 彼得佩依(貝利卡)"},
+        {huntingZoneId:  63,   templateId: 1281, name: "[63-1281] 彼得佩依(貝利卡)"},
+// {huntingZoneId:  63,   templateId: 2950, name: "[63-2950] 彼得佩依(貝利卡)"},
+// {huntingZoneId:  63,   templateId: 2951, name: "[63-2951] 彼得佩依(貝利卡)"},
+// {huntingZoneId:  63,   templateId: 2952, name: "[63-2952] 彼得佩依(貝利卡)"},
 
-{huntingZoneId:  72,   templateId: 1271, name: "[都市 72-1271] 修墨斯克(爾雷曼西亞)"},
-{huntingZoneId:  72,   templateId: 1278, name: "[都市 72-1278] 阿爾魯瑪(爾雷曼西亞)"},
+{huntingZoneId:  72,   templateId: 1271, name: "[72-1271] 修墨斯克(爾雷曼西亞)"},
+{huntingZoneId:  72,   templateId: 1278, name: "[72-1278] 阿爾魯瑪(爾雷曼西亞)"},
 
-{huntingZoneId:  84,   templateId: 1271, name: "[都市 84-1271] 凱爾門拉依特(凱亞多勒)"},
-{huntingZoneId:  84,   templateId: 1278, name: "[都市 84-1278] 凱依德拉(凱亞多勒)"},
+{huntingZoneId:  84,   templateId: 1271, name: "[84-1271] 凱爾門拉依特(凱亞多勒)"},
+{huntingZoneId:  84,   templateId: 1278, name: "[84-1278] 凱依德拉(凱亞多勒)"},
 
         // 特殊
         {huntingZoneId:  13,   templateId: 1271, name: "[巴其溫保護領地]巴勒圖(黎明庭園)"}
